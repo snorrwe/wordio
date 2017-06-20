1fa5c61580dc9e2e76d5c15d489a45a7d97458d3
Daniel Kiss <littlesnorrboy@gmail.com>
2017-06-20 23:05:04 +0200

    Fix broken unit tests

--- webclient/src/app/app.component.spec.ts
+++ webclient/src/app/app.component.spec.ts
@@ -1,12 +1,22 @@
+import { Component, Input } from '@angular/core';
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+
+@Component({
+  selector: 'wordio-board'
+  , template: ''
+})
+class BoardMock {
+  @Input('board') public board: any;
+}
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
+        , BoardMock
       ],
     }).compileComponents();
   }));
--- webclient/src/app/services/http.service.spec.ts
+++ webclient/src/app/services/http.service.spec.ts
@@ -1,15 +1,18 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { Http } from '@angular/http';
 
 import { HttpService } from './http.service';
 
-describe('HttpService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [HttpService]
-    });
-  });
+var httpService: Http = {} as any;
 
-  it('should be created', inject([HttpService], (service: HttpService) => {
-    expect(service).toBeTruthy();
-  }));
+describe('HttpService', () => {
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			providers: [HttpService, { provide: Http, useValue: httpService}]
+		});
+	});
+
+	it('should be created', inject([HttpService], (service: HttpService) => {
+		expect(service).toBeTruthy();
+	}));
 });
