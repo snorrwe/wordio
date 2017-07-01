95bc7e666def6aab2aedcab686d9645c20bee614
Daniel Kiss <littlesnorrboy@gmail.com>
2017-07-01 17:22:39 +0200

    Add missing copy to deploy.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -5,6 +5,7 @@ sudo apt-get install apache2
 python -m pip install --no-cache-dir -r ./requirements.txt
 
 sudo cp ./conf/httpd.conf /etc/httpd/conf/
+sudo cp -r ./app /var/www/wordio
 sudo apache2ctl configtest
 sudo systemctl restart apache2
 
