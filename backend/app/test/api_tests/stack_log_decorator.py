a3faa7f70b71540daeef40a23b17b65a15f8d227
Daniel Kiss <littlesnorrboy@gmail.com>
2017-07-02 20:21:51 +0200

    Fix logging in the decorator

--- backend/app/test/api_tests/stack_log_decorator.py
+++ backend/app/test/api_tests/stack_log_decorator.py
@@ -2,11 +2,16 @@ functionStack = []
 def log_stack(name):
     def inner_decorator(method):
         def wrapper(*args, **kwargs):
-            print("%s_____ %s starting _____" % (''.join([' ' for i in range(len(functionStack) * 5)]), name))
-            functionStack.append("")
-            result = method(*args, **kwargs)
-            functionStack.pop()
-            print("%s_____ %s finished _____\n" % (''.join([' ' for i in range(len(functionStack) * 5)]), name))
+            try:
+                print("%s_____ %s starting _____" % (''.join([' ' for i in range(len(functionStack) * 5)]), name))
+                functionStack.append("")
+                return method(*args, **kwargs)
+            except:
+                print("%s_____ %s error _____\n" % (''.join([' ' for i in range(len(functionStack) * 5)]), name))
+                raise
+            finally:
+                functionStack.pop()
+                print("%s_____ %s finished _____\n" % (''.join([' ' for i in range(len(functionStack) * 5)]), name))
             return result
         return wrapper
     return inner_decorator
