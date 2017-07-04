689b0393ee620eb833e9a3ac61fe47c71025143f
Daniel Kiss <littlesnorrboy@gmail.com>
2017-07-04 18:56:04 +0200

    Add custom tabChar support

--- backend/app/test/api_tests/stack_log_decorator.py
+++ backend/app/test/api_tests/stack_log_decorator.py
@@ -1,19 +1,20 @@
-def log(name, message, tabs):
-    print("%s____ %s %s ____" % (''.join([' ' for i in range(4 * tabs)]), name, message))
+def log(name, message, tabs, tabChar):
+    print("%s  %s %s " % (''.join([tabChar for i in range(4 * tabs)]), name, message))
 
 functionStack = []
-def log_stack(name):
+def log_stack(name, **kwargs):
+    tabChar = kwargs['tabChar'] if 'tabChar' in kwargs else ' '
     def inner_decorator(method):
         def wrapper(*args, **kwargs):
             try:
-                log(name, "starting", len(functionStack))
-                functionStack.append("")
+                log(name, "starting", len(functionStack), tabChar)
+                functionStack.append(1)
                 return method(*args, **kwargs)
             except:
-                log(name, "error", len(functionStack) - 1)
+                log(name, "error", len(functionStack) - 1, tabChar)
                 raise
             finally:
                 functionStack.pop()
-                log(name, "finished", len(functionStack))
+                log(name, "finished", len(functionStack), tabChar)
         return wrapper
     return inner_decorator
