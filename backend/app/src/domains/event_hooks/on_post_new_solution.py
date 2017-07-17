d10fc31e7dbc786b3662ae1649f675a9bc65dbcc
Daniel Kiss <littlesnorrboy@gmail.com>
2017-07-17 17:55:15 +0200

    Fix misstyped query

--- backend/app/src/domains/event_hooks/on_post_new_solution.py
+++ backend/app/src/domains/event_hooks/on_post_new_solution.py
@@ -5,7 +5,7 @@ def remove_old_solutions(solution):
     current_app.data.driver.db['solutions'].delete_many({
         'game': ObjectId(solution['game'])
         , 'nickname': solution['nickname']
-        , '_updated': { '$le': solution['_updated']  }
+        , '_updated': { '$lt': solution['_updated']  }
     })
 
 def on_post_new_solution(solutions):
