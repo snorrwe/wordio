a9787d97d1673b6f2bbcabf199986826bb82d09b
Daniel Kiss <littlesnorrboy@gmail.com>
2017-07-17 23:41:51 +0200

    Add getGames()

--- webclient/src/app/services/board.service.ts
+++ webclient/src/app/services/board.service.ts
@@ -1,10 +1,30 @@
 import { Injectable } from "@angular/core";
 import { EveHttpService, ItemsDto } from "./http.service";
+import { Urls } from './http/urls';
 
 import { Tile } from "../models/tile";
+import { GameDto, Game } from "../models/game";
 
 @Injectable()
 export class BoardService {
 
     constructor(private httpService: EveHttpService) { }
+
+    getGames(): Promise<Game[]> {
+        return this.httpService.get<ItemsDto<GameDto>>(this.url(Urls.GAMES))
+            .then(response => {
+                let result: Game[] = [];
+                for (let game of response._items) {
+                    result.push(new Game(game));
+                }
+                return result;
+            });
+    }
+
+    private url(url: string) {
+        let result = Urls.API_BASE_URL + url;
+        while (result.indexOf("//") > -1) result.replace("//", "/");
+        result += "/";
+        return result;
+    }
 }
