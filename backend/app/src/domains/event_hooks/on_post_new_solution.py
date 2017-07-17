3718c0d53033acb555f10158c5ba0b348725e42b
kid <kid@KISSDANI>
2017-07-17 12:40:12 +0200

    Resolve conflicts

--- backend/app/src/domains/event_hooks/on_post_new_solution.py
+++ backend/app/src/domains/event_hooks/on_post_new_solution.py
@@ -1,15 +1,8 @@
 from flask import current_app
 from bson.objectid import ObjectId
 
-<<<<<<< HEAD
 def remove_old_solutions(solution):
     current_app.data.driver.db['solutions'].delete_many({
-=======
-db = current_app.data.driver.db
-
-def remove_old_solutions(solution):
-    db['solutions'].delete_many({
->>>>>>> 3b60499... Modify solution concurency control to hook to the inserted event
         'game': ObjectId(solution['game'])
         , 'nickname': solution['nickname']
         , '_updated': { '$le': solution['_updated']  }
