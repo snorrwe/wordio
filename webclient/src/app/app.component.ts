c888ac01ba4d7a8ffd0aef5a85c2c781cd87a006
Daniel Kiss <littlesnorrboy@gmail.com>
2017-07-18 18:18:46 +0200

    Remove dummy index page

--- webclient/src/app/app.component.ts
+++ webclient/src/app/app.component.ts
@@ -6,16 +6,6 @@ import { Component } from "@angular/core";
     styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-    private dummyboard: any[][];
 
-    constructor() {
-        this.dummyboard = [];
-        for (let i = 5; i >= 0; i--) {
-            const next = [];
-            for (let j = 5; j >= 0; j--) {
-                next.push({ value: 10 * i + 1 + j });
-            }
-            this.dummyboard.push(next);
-        }
-    }
+    constructor() { }
 }
