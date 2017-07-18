57936ee13a0e080d0f9c26f72d73b10e4f4538c7
kid <kid@KISSDANI>
2017-07-18 11:09:04 +0200

    Add tests for makeDto

--- webclient/src/app/models/game.spec.ts
+++ webclient/src/app/models/game.spec.ts
@@ -16,61 +16,86 @@ export function assertBoardIsValid(board: Tile[][], columns: number, rows: numbe
 }
 
 describe("Game tests", () => {
-    it("Can be created by sorted board", () => {
-        const dto: GameDto = {
-            board: [
-                { x: 0, y: 0 }
-                , { x: 0, y: 1 }
-                , { x: 1, y: 0 }
-                , { x: 1, y: 1 }
-            ]
-        } as any;
-        const result = new Game(dto);
-        expect(result).toBeTruthy();
-        assertBoardIsValid(result.board, 2, 2);
+    describe("constructor tests", () => {
+        it("Can be created by sorted board", () => {
+            const dto: GameDto = {
+                board: [
+                    { x: 0, y: 0 }
+                    , { x: 0, y: 1 }
+                    , { x: 1, y: 0 }
+                    , { x: 1, y: 1 }
+                ]
+            } as any;
+            const result = new Game(dto);
+            expect(result).toBeTruthy();
+            assertBoardIsValid(result.board, 2, 2);
+        });
+
+        it("Can be created by reversed board", () => {
+            const dto: GameDto = {
+                board: [
+                    { x: 1, y: 1 }
+                    , { x: 1, y: 0 }
+                    , { x: 0, y: 1 }
+                    , { x: 0, y: 0 }
+                ]
+            } as any;
+            const result = new Game(dto);
+            expect(result).toBeTruthy();
+            assertBoardIsValid(result.board, 2, 2);
+        });
+
+        it("Can be created by random board", () => {
+            const dto: GameDto = {
+                board: [
+                    { x: 0, y: 1 }
+                    , { x: 1, y: 0 }
+                    , { x: 1, y: 1 }
+                    , { x: 0, y: 0 }
+                ]
+            } as any;
+            const result = new Game(dto);
+            expect(result).toBeTruthy();
+            assertBoardIsValid(result.board, 2, 2);
+        });
+
+        it("Can be created by random board 2×3", () => {
+            const dto: GameDto = {
+                board: [
+                    { x: 0, y: 1 }
+                    , { x: 1, y: 0 }
+                    , { x: 1, y: 1 }
+                    , { x: 0, y: 0 }
+                    , { x: 2, y: 0 }
+                    , { x: 2, y: 1 }
+                ]
+            } as any;
+            const result = new Game(dto);
+            expect(result).toBeTruthy();
+            assertBoardIsValid(result.board, 3, 2);
+        });
     });
 
-    it("Can be created by reversed board", () => {
-        const dto: GameDto = {
-            board: [
-                { x: 1, y: 1 }
-                , { x: 1, y: 0 }
-                , { x: 0, y: 1 }
-                , { x: 0, y: 0 }
-            ]
-        } as any;
-        const result = new Game(dto);
-        expect(result).toBeTruthy();
-        assertBoardIsValid(result.board, 2, 2);
-    });
+    describe("makeDto tests", () => {
+        it("null returns null", () => {
+            const result = Game.makeDto(null);
+            expect(result).toBeNull();
+        });
 
-    it("Can be created by random board", () => {
-        const dto: GameDto = {
-            board: [
-                 { x: 0, y: 1 }
-                , { x: 1, y: 0 }
-                , { x: 1, y: 1 }
-                , { x: 0, y: 0 }
-            ]
-        } as any;
-        const result = new Game(dto);
-        expect(result).toBeTruthy();
-        assertBoardIsValid(result.board, 2, 2);
-    });
+        it("converts correctly", () => {
+            const dto: GameDto = {
+                host: { displayName: "" }
+                , board: [{ x: 0, y: 0 } as any, { x: 1, y: 0 } as any, { x: 0, y: 1 } as any, { x: 1, y: 1 } as any]
+                , _id: "kanga"
+                , _etag: "asd568dfns"
+                , _updated: new Date(1)
+                , _created: new Date(1)
+            }
+            const game = new Game(dto);
+            const result = Game.makeDto(game);
 
-    it("Can be created by random board 2×3", () => {
-        const dto: GameDto = {
-            board: [
-                { x: 0, y: 1 }
-                , { x: 1, y: 0 }
-                , { x: 1, y: 1 }
-                , { x: 0, y: 0 }
-                , { x: 2, y: 0 }
-                , { x: 2, y: 1 }
-            ]
-        } as any;
-        const result = new Game(dto);
-        expect(result).toBeTruthy();
-        assertBoardIsValid(result.board, 3, 2);
+            expect(result).not.toEqual(game);
+            expect(result).toEqual(dto);
+        });
     });
 });
