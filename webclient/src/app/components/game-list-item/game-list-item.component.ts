fbf56fd633ed6a72662c698f4d5a3b7467d8cd95
Daniel Kiss <littlesnorrboy@gmail.com>
2017-07-19 16:47:05 +0200

    Add navigation to game-list-item

--- webclient/src/app/components/game-list-item/game-list-item.component.ts
+++ webclient/src/app/components/game-list-item/game-list-item.component.ts
@@ -1,11 +1,21 @@
 import { Component, Input } from "@angular/core";
 import { Game } from "../../models/game";
 
+import { NavigationService } from "../../services/navigation.service";
+
 @Component({
-  selector: "wordio-game-list-item",
-  templateUrl: "./game-list-item.component.html",
-  styleUrls: ["./game-list-item.component.css"]
+    selector: "wordio-game-list-item",
+    templateUrl: "./game-list-item.component.html",
+    styleUrls: ["./game-list-item.component.css"]
 })
 export class GameListItemComponent {
     @Input("game") private game: Game;
+
+    constructor(private navigationService: NavigationService) { }
+
+    onClick() {
+        if (this.game && this.game._id) {
+            this.navigationService.push("games/" + this.game._id);
+        }
+    }
 }
