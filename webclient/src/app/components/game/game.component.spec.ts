be83e3e773e2e0ec6e725db951108f1d05847067
Daniel Kiss <littlesnorrboy@gmail.com>
2017-07-19 17:42:36 +0200

    Fix game tests

--- webclient/src/app/components/game/game.component.spec.ts
+++ webclient/src/app/components/game/game.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from "@angular/router";
 import { Directive, Input } from '@angular/core';
 import { GameComponent } from './game.component';
+import { GameService } from '../../services/game.service';
 
 @Directive({ selector: "[loaded]" })
 class LoadedMockDirective {
@@ -10,16 +12,25 @@ class LoadedMockDirective {
 describe('GameComponent', () => {
     let component: GameComponent;
     let fixture: ComponentFixture<GameComponent>;
+    let gameService: GameService;
+    let activatedRoute: ActivatedRoute;
 
     beforeEach(async(() => {
+        activatedRoute = {
+            params: {
+                subscribe: () => { }
+            }
+        } as any;
         TestBed.configureTestingModule({
-            declarations: [GameComponent, LoadedMockDirective]
+            declarations: [GameComponent, LoadedMockDirective],
+            providers: [{ provide: GameService, useValue: {} }, { provide: ActivatedRoute, useValue: activatedRoute }]
         }).compileComponents();
     }));
 
     beforeEach(() => {
         fixture = TestBed.createComponent(GameComponent);
         component = fixture.componentInstance;
+        gameService = (component as any).gameService;
         fixture.detectChanges();
     });
 
