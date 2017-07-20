d3abf54470ee96c7cd9409c57ccdf9b64130a5a4
Daniel Kiss <littlesnorrboy@gmail.com>
2017-07-20 10:20:22 +0200

    Add basics to Tile

--- webclient/src/app/components/tile/tile.component.ts
+++ webclient/src/app/components/tile/tile.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'wordio-tile',
-  templateUrl: './tile.component.html',
-  styleUrls: ['./tile.component.css']
+    selector: 'wordio-tile',
+    templateUrl: './tile.component.html',
+    styleUrls: ['./tile.component.css']
 })
-export class TileComponent implements OnInit {
+export class TileComponent {
 
-  constructor() { }
+    @Input("value") private value: string;
+    @Input("isSelected") private isSelected: boolean;
+    @Output("onSelectedChange") private onSelectedChangeEmitter = new EventEmitter<boolean>();
 
-  ngOnInit() {
-  }
+    constructor() { }
 
+    onClick(){
+        this.isSelected = !this.isSelected;
+        this.onSelectedChangeEmitter.emit(this.isSelected);
+    }
 }
