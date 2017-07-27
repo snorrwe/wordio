ffabd56b8702856ac9183a543f490ab2e3a5152a
Daniel Kiss <littlesnorrboy@gmail.com>
2017-07-28 00:35:53 +0200

    Refactor bootstrapping

--- backend/app/src/main.py
+++ backend/app/src/main.py
@@ -4,55 +4,43 @@ import pymongo
 from eve import Eve
 from flask import current_app
 from flask import request, abort, make_response, jsonify
-from src.user import register, login
+from flask_cors import CORS
+from src.user.register import register as register_enpoint
+from src.user.login import login as login_enpoint
 from src.auth.auth import WordioAuth
 from src.utility.logger import LogService
 from src.domains.event_hooks.insert_tiles_by_games import insert_tiles_by_games
 from src.domains.event_hooks.on_new_solution import on_new_solution
 from src.domains.event_hooks.on_post_new_solution import on_post_new_solution
-from src.custom.hello_endpoint import hello
+from src.custom.hello_endpoint import hello as hello_endpoint
 from src import settings
 SETTINGS_PATH = os.path.abspath(settings.__file__)
 
-def enable_cors(app):
-    try:
-        from flask_cors import CORS
-        CORS(app)
-    except ImportError:
-        pass
+def bootstrap_app(app):
+    CORS(app)
+    @app.route('/hello', methods=['GET'])
+    def hello(): return hello_endpoint()
+    @app.route('/login', methods=['POST'])
+    def login(): return login_enpoint(app)
+    @app.route('/register', methods=['POST'])
+    def register(): return register_enpoint(app)
+    app.on_pre_POST_games += insert_tiles_by_games
+    app.on_pre_POST_solutions += on_new_solution
+    app.on_inserted_solutions += on_post_new_solution
 
-def start_app(app):
+def main():
     try:
         logger = LogService("log.txt")
-        logger.debug("Starting")
-        enable_cors(app)
+        logger.debug("---------Starting----------")
+        app = Eve(auth = WordioAuth, settings = SETTINGS_PATH)
+        bootstrap_app(app)
         app.run()
     except:
         error = sys.exc_info()[0]
-        print("Error in main %s" % error)
+        print("Error in main %s" % (error))
+        print(str(error))
         if logger:
             logger.error(error)
-
-app = Eve(auth = WordioAuth, settings = SETTINGS_PATH)
-
-@app.route('/hello', methods=['GET'])
-def hello_controller():
-    return hello()
-
-@app.route('/login', methods=['POST'])
-def login_controller():
-    return login.login(app)
-
-@app.route('/register', methods=['POST'])
-def register_controller():
-    return register.register(app)
-
-app.on_pre_POST_games += insert_tiles_by_games
-app.on_pre_POST_solutions += on_new_solution
-app.on_inserted_solutions += on_post_new_solution
-
-def main():
-    start_app(app)
 
 if __name__ == '__main__':
     main()
