73117964e97ddfb7c578958fe76a3a4542ca8b37
Daniel Kiss <littlesnorrboy@gmail.com>
2017-07-28 20:00:52 +0200

    Add login implementation

--- webclient/src/app/components/login/login.component.spec.ts
+++ webclient/src/app/components/login/login.component.spec.ts
@@ -1,25 +1,46 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Pipe, PipeTransform } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { LoginComponent } from './login.component';
+import { AuthenticationService } from '../../services/authentication.service';
+import { NavigationService } from '../../services/navigation.service';
+
+@Pipe({
+    name: "translate"
+})
+class TranslateMockPipe implements PipeTransform {
+    transform(value) { return value; }
+}
 
 describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+    let component: LoginComponent;
+    let fixture: ComponentFixture<LoginComponent>;
+    let authServiceMock: AuthenticationService;
+    let navServiceMock: NavigationService;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        let authService = {};
+        TestBed.configureTestingModule({
+            declarations: [LoginComponent, TranslateMockPipe],
+            providers: [
+                { provide: AuthenticationService, useValue: { canActivate: () => {} } }
+                , { provide: NavigationService, useValue: {} }
+            ],
+            imports: [FormsModule]
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(LoginComponent);
+        component = fixture.componentInstance;
+        authServiceMock = (component as any).authService;
+        navServiceMock = (component as any).navService;
+        fixture.detectChanges();
+    });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(component).toBeTruthy();
+        expect(authServiceMock).toBeTruthy();
+        expect(navServiceMock).toBeTruthy();
+    });
 });
--- webclient/src/app/components/login/login.component.ts
+++ webclient/src/app/components/login/login.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthenticationService } from '../../services/authentication.service';
+import { NavigationService } from '../../services/navigation.service';
+
 @Component({
     selector: 'wordio-login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+    private username: string;
+    private password: string;
 
-    constructor() { }
+    private usernameErrors: string[] = []
+    private passwordErrors: string[] = []
+
+    constructor(private authService: AuthenticationService, private navService: NavigationService) { }
 
     ngOnInit() {
+        if (this.authService.canActivate()) {
+            this.navService.pop()
+        }
     }
 
+    login() {
+        this.usernameErrors = [];
+        this.passwordErrors = [];
+        this.authService.login(this.username, this.password)
+            .then(result => {
+                if (result) {
+                    this.navService.pop();
+                }
+            })
+            .catch(error => {
+                console.error("loginerror", error);
+            });
+    }
 }
