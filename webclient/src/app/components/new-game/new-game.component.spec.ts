ce18a2647374f388169439f49c0f6d6c4a0e02a6
Daniel Kiss <littlesnorrboy@gmail.com>
2017-07-29 00:14:29 +0200

    Add board layout

--- webclient/src/app/components/new-game/new-game.component.spec.ts
+++ webclient/src/app/components/new-game/new-game.component.spec.ts
@@ -1,17 +1,49 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { Component, Pipe, Input, Output, EventEmitter } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 import { NewGameComponent } from "./new-game.component";
 import { GameService } from "../../services/game.service";
+
+@Pipe({ name: "translate" })
+class TranslateMockPipe {
+    transform(value) { return value; }
+}
+
+@Component({ selector: "wordio-board", template: "" })
+class WordioBoardMock {
+    static instance: WordioBoardMock;
+
+    @Input("board") private board: any;
+    @Output("onChange") private onChangeEmitter = new EventEmitter<any>();
+    @Output("onTileSelect") private onTileSelectEmitter = new EventEmitter<any>();
+
+    constructor() {
+        WordioBoardMock.instance = this;
+    }
+}
 
 describe("NewGameComponent", () => {
     let component: NewGameComponent;
     let fixture: ComponentFixture<NewGameComponent>;
     let gameService: GameService;
+    let board: WordioBoardMock;
+
+    afterAll(() => {
+        if (fixture) {
+            fixture.destroy();
+        }
+    });
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [NewGameComponent],
-            providers: [{ provide: GameService, useValue: {} }]
+            declarations: [
+                NewGameComponent
+                , WordioBoardMock
+                , TranslateMockPipe
+            ],
+            providers: [{ provide: GameService, useValue: {} }],
+            imports: [FormsModule]
         }).compileComponents();
     }));
 
@@ -20,10 +52,12 @@ describe("NewGameComponent", () => {
         component = fixture.componentInstance;
         gameService = (component as any).gameService;
         fixture.detectChanges();
+        board = WordioBoardMock.instance;
     });
 
     it("should be created", () => {
         expect(component).toBeTruthy();
         expect(gameService).toBeTruthy();
+        expect(board).toBeTruthy();
     });
 });
--- webclient/src/app/components/new-game/new-game.component.ts
+++ webclient/src/app/components/new-game/new-game.component.ts
@@ -1,12 +1,127 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { GameService } from "../../services/game.service";
+import { Tile } from "../../models/tile";
+
+function hashBoard(board: Tile[][]): string {
+    let result = "";
+    board.forEach(column => {
+        if (!column || !column.length) return;
+        column.forEach(tile => {
+            result += tile.value + " ";
+        });
+        result += "\n";
+    });
+    return result;
+}
+
+function parseBoard(board: string): { board: Tile[][], columns: number, rows: number } {
+    const lines = board.split("\n");
+    const matrix = [];
+    for (const line of lines) {
+        const items = line.split(/[ ,;]+/).filter(value => value != null && value.length === 1);
+        if (items.length) matrix.push(items);
+    }
+    const result = [];
+    let columns = 0;
+    let rows = 0;
+    for (let i = 0; i < matrix.length; i++) {
+        if (i > rows) rows = i;
+        const line: Tile[] = [];
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (!matrix[i][j]) continue;
+            if (j > columns) columns = j;
+            line.push({
+                x: i,
+                y: j,
+                value: matrix[i][j]
+            });
+        }
+        result.push(line);
+    }
+    return {
+        board: result,
+        columns: columns + 1,
+        rows: rows + 1
+    };
+}
 
 @Component({
     selector: "wordio-new-game",
     templateUrl: "./new-game.component.html",
     styleUrls: ["./new-game.component.scss"]
 })
-export class NewGameComponent {
+export class NewGameComponent implements OnInit {
+    private _board: Tile[][] = [];
+    get board() { return this._board; }
+    set board(value) {
+        this._board = value;
+        this.handleBoardChange();
+    }
+
+    private _columns: number = 2;
+    get columns() { return this._columns; }
+    set columns(value) {
+        this._columns = +value;
+        this.handleParamsChange();
+    }
+
+    private _rows: number = 2;
+    get rows() { return this._rows; }
+    set rows(value) {
+        this._rows = +value;
+        this.handleParamsChange();
+    }
+
+    private _boardHash: string
+    get boardHash() { return this._boardHash; }
+    set boardHash(value) { this._boardHash = value; }
 
     constructor(private gameService: GameService) { }
+
+    ngOnInit() {
+        this.handleParamsChange();
+    }
+
+    submit() {
+        throw "Not implemented";
+    }
+
+    private handleParamsChange() {
+        const board = [];
+        for (let i = 0; i < this.rows; ++i) {
+            const line = (this.board[i] && this.board[i].filter((v, i) => i < this.columns)) || [];
+            for (let j = line.length; j < this.columns; ++j) {
+                const value = this.getCharByPosition(i, j);
+                line.push({
+                    x: j,
+                    y: i,
+                    filled: false,
+                    value: value
+                });
+            }
+            board.push(line);
+        }
+        this.board = board;
+    }
+
+    private getCharByPosition(x: number, y: number) {
+        const alphabetLength = ('Z'.charCodeAt(0) - 'A'.charCodeAt(0));
+        return String.fromCharCode((x * this.rows + y) % alphabetLength + 'A'.charCodeAt(0));
+    }
+
+    onTileSelect(event: { tile: Tile, mouseEvent: MouseEvent }) {
+        console.log(event, this.board[event.tile.y][event.tile.x]);
+    }
+
+    private handleBoardChange() {
+        this.boardHash = hashBoard(this.board);
+    }
+
+    onHashChange() {
+        const result = parseBoard(this.boardHash);
+        this.columns = result.columns;
+        this.rows = result.rows;
+        this.board = result.board;
+        this.handleParamsChange();
+    }
 }
